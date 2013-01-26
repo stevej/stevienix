5edde3c5f5f5e2c01968b0f0aeece03427b28314
Steve Jenson <stevej@fruitless.org>
2013-01-25 20:30:52 -0800

    no need to pass the initrd to qemu, we mount it in the kernel image

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
 
-#cat stage1 stage2 pad src/kernel.bin > floppy.img
-qemu-system-i386 -cpu pentium -kernel src/kernel.bin -initrd initrd.img -m 32 -monitor stdio -d int,in_asm -D qemu.log
-
+qemu-system-i386 -cpu pentium -kernel src/kernel.bin -m 32 -monitor stdio -d int,in_asm -D qemu.log
