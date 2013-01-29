03073b3a6989b75cf5d5bc7b936ce38576536f2c
Steve Jenson <stevej@fruitless.org>
2013-01-29 13:37:37 -0800

    turn on cpu logging

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-qemu-system-i386 -cpu pentium -kernel src/kernel.bin -m 32 -monitor stdio -D qemu.log
+qemu-system-i386 -cpu pentium -kernel src/kernel.bin -m 32 -monitor stdio -d int,in_asm,cpu_reset -D qemu.log
