967debeafb3dc77d554e15e1cd9c04a75f1e0075
Steve Jenson <stevej@fruitless.org>
2013-11-20 14:00:28 -0800

    Enable gdb debugging port

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 . build_floppy.sh
-qemu-system-i386 -cpu pentium -fda floppy.img -m 48 -monitor stdio -usb -usbdevice mouse -d int,in_asm,cpu_reset -D qemu.log
+qemu-system-i386 -s -cpu pentium -fda floppy.img -m 48 -monitor stdio -usb -usbdevice mouse -d int,in_asm,cpu_reset -D qemu.log
