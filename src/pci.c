2f1770c4012cd6e4f2d0832dfb004ca916d9da48
Steve Jenson <stevej@fruitless.org>
2013-11-20 11:03:03 -0800

    Improved scanning of PCI bus
    And build the full PCI header for each device found.

--- src/pci.c
+++ src/pci.c
@@ -21,6 +21,11 @@ u16 pci_config_read (u32 bus, u32 device, u32 func, u32 offset) {
   return (u16)((inl(0xCFC) >> ((offset & 2) * 8)) & 0xffff);
 }
 
+u8 pci_exists(u32 bus, u32 slot, u32 func) {
+  u16 vendor_code = pci_config_read(bus, slot, func, 0);
+  return (vendor_code != 0xFFFF);
+}
+
 void print_pci_entry(PCI_VENTABLE vendor, PCI_DEVTABLE device) {
   printk("%x:%x %s %s\n", vendor.VenId, device.DevId, vendor.VenFull, device.ChipDesc);
 }
@@ -46,24 +51,78 @@ PCI_DEVTABLE lookup_device(u16 vendorID, u16 deviceID) {
   return (PCI_DEVTABLE){vendorID, deviceID, "Unknown", "Unknown"};
 }
 
-void print_pci(u16 bus, u16 device, u16 func) {
-  u16 vendor_code = pci_config_read(bus, device, func, 0);
+void print_pci_header_nice(struct pci_header *header) {
+  print_pci_entry(lookup_vendor(header->vendor_id), lookup_device(header->vendor_id, header->device_id));
+}
 
-  if (vendor_code != 0xFFFF) {
-    // Valid device found
-    printk("%d:%d:%d ", bus, device, func);
-    u16 device_code = pci_config_read(bus, device, func, 2);
-    print_pci_entry(lookup_vendor(vendor_code), lookup_device(vendor_code, device_code));
-  }
+void print_pci_header_full(struct pci_header *header) {
+  printk("DEVICE: vendor_id:%x device_id:%x command:%x status:%x revision_id:%x prog_if:%x subclass:%x \
+class_code:%x cache_line_size:%x latency_timer:%x header_type:%x bist:%x \
+bar[0]:%x bar[1]:%x bar[2]:%x bar[3]:%x bar[4]:%x cardbus_cis_ptr:%x \
+subsystem_id:%x subsystem_vendor_id:%x rom_base_address:%x capabilities:%x \
+interrupt_line:%x interrupt_pin:%x min_grant:%x max_latency:%x\n",
+         header->vendor_id,
+         header->device_id,
+         header->command,
+         header->prog_if,
+         header->subclass,
+         header->class_code,
+         header->cache_line_size,
+         header->latency_timer,
+         header->header_type,
+         header->bist,
+         header->bar[0],
+         header->bar[1],
+         header->bar[2],
+         header->bar[3],
+         header->bar[4],
+         header->cardbus_cis_ptr,
+         header->subsystem_id,
+         header->subsystem_vendor_id,
+         header->rom_base_address,
+         header->capabilities,
+         header->interrupt_line,
+         header->interrupt_pin,
+         header->min_grant,
+         header->max_latency
+         );
 }
 
 
+
+/**
+ * From the bus, slot, and function, read the PCI header data
+ * into *header
+ */
+void pci_read_header(u32 bus, u32 slot, u32 func, struct pci_header *header) {
+  u32 addr;
+  u32 *header_data = (void*) header;
+
+  addr = (bus << 16) | (slot << 11) | (func << 8) | 0x80000000;
+
+  for (u32 i = 0; i < 15; i++) {
+    outl(0xCF8, addr | (i << 2));
+    header_data[i] = inl(0xCFC);
+  }
+}
+
+/**
+ * Does a brute-force enumeration of the PCI bus and prints a nice form of each header found.
+ *
+ * TODO(stevej): build a device list.
+ * TODO(stevej): configure bus-to-bus devices for forwarding.
+ */
 void scan_pci_bus() {
   printk("PCI bus found! enumerating bus for devices\n");
   for (u16 bus = 0; bus < 256; bus++) {
     for (u16 device = 0; device < 32; device++) {
       for (u8 func = 0; func < 8; func++) {
-        print_pci(bus, device, func);
+        if (pci_exists(bus, device, func)) {
+          struct pci_header *header = kmalloc(sizeof(pci_header));
+          pci_read_header(bus, device, func, header);
+          printk("[%x:%x:%x] ", bus, device, func);
+          print_pci_header_nice(header);
+        }
       }
     }
   }
--- src/pci.h
+++ src/pci.h
@@ -3,6 +3,34 @@
 
 #include "types.h"
 #include "screen.h"
+#include "kheap.h"
+
+struct pci_header {
+  u16 vendor_id;
+  u16 device_id;
+  u16 command;
+  u16 status;
+  u8  revision_id;
+  u8  prog_if;
+  u8  subclass;
+  u8  class_code;
+  u8  cache_line_size;
+  u8  latency_timer;
+  u8  header_type;
+  u8  bist;
+  u32 bar[5];
+  u32 cardbus_cis_ptr;
+  u16 subsystem_id;
+  u16 subsystem_vendor_id;
+  u32 rom_base_address;
+  u8  capabilities;
+  u8  reserved0[3];
+  u32 reserved1;
+  u8  interrupt_line;
+  u8  interrupt_pin;
+  u8  min_grant;
+  u8  max_latency;
+} pci_header;
 
 void scan_pci_bus();
 
