0a2da5fba68cae07d6bbc87337776be0b133beda
Steve Jenson <stevej@fruitless.org>
2013-12-02 15:27:14 -0800

    Generate a makeshift kernel map.

--- make_image.sh
+++ make_image.sh
@@ -11,6 +11,7 @@ cp ${BUILDDIR}/grub/* ${BUILDDIR}/scratch/cd/boot/grub
 #cp ${BUILDDIR}/run/boot.tar ${BUILDDIR}/run/cd/boot.tar
 #cp ${BUILDDIR}/sbin/init ${BUILDDIR}/run/cd/init
 #cp ${BUILDDIR}/lib/dl.so ${BUILDDIR}/run/cd/dl.so
+cp ${BUILDDIR}/src/kernel.map ${BUILDDIR}/scratch/cd
 cp ${BUILDDIR}/src/kernel.elf ${BUILDDIR}/scratch/cd
 
 mkisofs -vvv -R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -o stevienix.iso scratch/cd
