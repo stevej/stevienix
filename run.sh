6cf0e4142736157f56411921bd534ae3b1127681
Steve Jenson <stevej@fruitless.org>
2013-12-02 13:38:36 -0800

    remember to build source first

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
+pushd . && cd src && make && popd
 . make_image.sh
 qemu-system-i386 -s -cpu core2duo -cdrom stevienix.iso -vga vmware -m 128 -serial stdio -usb -usbdevice mouse -d int,in_asm,cpu_reset -D qemu.log
